eb1c081baa03201aabbe253616f97da119d72f98
yaseen <Iqbal>
2020-04-13 00:42:25 +0500

    fix(broswer): Resolve browser back button issue on chats route

--- app/assets/javascripts/chat_engine/chats.js
+++ app/assets/javascripts/chat_engine/chats.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+	if (window.history && window.history.pushState) {
+	    $(window).on('popstate', function() {
+	      location.reload(true)
+	    });
+	}
+});
